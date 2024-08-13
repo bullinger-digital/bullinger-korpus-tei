d8edc9deb8977d7e8dbda5b7125f0c04257b3907
bernsc <bernard.schroffenegger@uzh.ch>
2024-08-13 13:55:02 +0200

    bugfix

--- scripts/patch.py
+++ scripts/patch.py
@@ -10,5 +10,6 @@ for f in os.listdir(ROOT):
     if re.match(r'.*\.xml', f):
         p = os.path.join(ROOT, f)
         with open(p) as fi: s = fi.read()
-        s = re.sub(r'<placeName([^>]*>[^<]*</persName>)', r'<persName\1', s, flags=re.S)
+        s = re.sub(r'(<persName[^>]*ref=")l', r'\1p', s, flags=re.S)
+        s = re.sub(r'(<placeName ref=")p', r'\1l', s, flags=re.S)
         with open(p, 'w') as fo: fo.write(s)
