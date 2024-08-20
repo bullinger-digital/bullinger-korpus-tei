84fd2049b8788a25402f587e07aac0823bcaa379
bernsc <bernard.schroffenegger@uzh.ch>
2024-08-20 03:23:27 +0200

    H[^\s<]+ <pers

---
+++ scripts/patch_tagger_heinrich.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+import os, re
+
+""" adds footnote types (bio/lex) """
+
+heiri = []
+with open("scripts/src/heiri.csv") as fi:
+    heiri = [h.strip() for h in fi if h.strip()]
+
+# print(heiri)
+ROOT = "data/letters/"
+for f in os.listdir(ROOT):
+    if re.match(r'.*\.xml', f):
+        p = os.path.join(ROOT, f)
+        with open(p) as fi: s = fi.read()
+        for h in heiri:
+            s = re.sub(r'('+re.escape(h)+r'\s*)(<persName[^>]*ref="p495"[^>]*>)', r'\2\1', s, flags=re.S)
+        with open(p, 'w') as fo: fo.write(s)
