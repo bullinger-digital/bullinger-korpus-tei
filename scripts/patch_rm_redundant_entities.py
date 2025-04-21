7561cbc5a6dcb9e77c39b8de6a6a8eafab101d92
bernsc <bernard.schroffenegger@uzh.ch>
2025-04-22 01:24:06 +0200

    removed redundant entity-notes

--- scripts/patch_rm_redundant_entities.py
+++ scripts/patch_rm_redundant_entities.py
@@ -9,6 +9,6 @@ for f in sorted(os.listdir(ROOT)):
     if f.endswith('.xml'):
         p = os.path.join(ROOT, f)
         with open(p) as fi: s = fi.read()
-        for e in re.findall(r'(<note type="entity">(.*?)</note>)', s, flags=re.S):
+        for e in re.findall(r'(<note type="entity">(<.*?>)</note>)', s, flags=re.S):
             s = re.sub(re.escape(e[1]+e[0]), e[1], s, flags=re.S)
         with open(p, 'w') as fo: fo.write(s)
