7d5b61e799943e8f3e8d5e6213ace4e2c92a17e2
bernsc <bernard.schroffenegger@uzh.ch>
2025-04-22 03:16:34 +0200

    small corrections

--- scripts/mods.py
+++ scripts/mods.py
@@ -4,36 +4,12 @@
 import re, os
 
 
-def apply(mod_func, root):
-    for f in os.listdir(root):
-        if f != ".DS_Store":
-            fp = os.path.join(root, f)
-            with open(fp) as fi: s = fi.read()
-            s_new = mod_func(s)
-            if s_new != s:
-                with open(fp, 'w') as fo: fo.write(s_new)
-                print(fp, "updated.")
-
-def mods(s: str):
-    mappings = [
-        ["</?ipron>", r""]
-    ]
-    for m in mappings:
-        if m: s = re.sub(m[0], m[1], s, flags=re.S)
-    return s
-
-
-
 if __name__ == '__main__':
 
     for f in os.listdir("data/letters/"):
-        if f != ".DS_Store":
+        if f.endswith('.xml'):
             fp = os.path.join("data/letters/", f)
             if not os.path.isdir(fp) and os.path.isfile(fp):
                 with open(fp) as fi: s = fi.read()
-                s = re.sub(r'<placeName ref="l587" type="auto_name">(\(Zürich)</placeName>', r'\1', s, flags=re.S)
-                with open(fp, 'w') as fo: fo.write(s)
-                print(fp, "updated.")
-
-    #apply(mods, "data/letters/")
-    print("Korpus aktualisiert.")
+                #s = re.sub(r'', r'', s, flags=re.S)
+                #with open(fp, 'w') as fo: fo.write(s)
