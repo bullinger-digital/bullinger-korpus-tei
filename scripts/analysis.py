281d8b2f6f6c1804a7c95caadb431c421006832e
bernsc <bernard.schroffenegger@uzh.ch>
2025-05-04 16:59:57 +0200

    added hidded letter-text from hbbw-21

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -147,4 +147,4 @@ if __name__ == '__main__':
     x.space_sub = r' '
     x.rm_untagged_fls = False  # \*\[\*.*?\]
 
-    x.search(["data/letters"], r'<note type="entity".*?</note>')
+    x.search(["data/letters"], r'<for')
