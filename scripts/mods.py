203178c210c56cb71a25cd5b7083cc797c1694ae
bernsc <bernard.schroffenegger@uzh.ch>
2025-05-15 19:27:00 +0200

    corrected invalid place-IDs

--- scripts/mods.py
+++ scripts/mods.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
             fp = os.path.join("data/letters/", f)
             if not os.path.isdir(fp) and os.path.isfile(fp):
                 with open(fp) as fi: s = fi.read()
-                s = re.sub(r'"l472"', "l471", s, flags=re.S)
+                s = re.sub(r'ref=l471', r'ref="l471"', s, flags=re.S)
                 with open(fp, 'w') as fo: fo.write(s)
