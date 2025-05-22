67f63a152472475e33f4dbd2da98efc8901481d6
bernsc <bernard.schroffenegger@uzh.ch>
2025-05-22 16:04:07 +0200

    replaced multiple blanks by single blanks

--- scripts/mods.py
+++ scripts/mods.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
             fp = os.path.join("data/letters/", f)
             if not os.path.isdir(fp) and os.path.isfile(fp):
                 with open(fp) as fi: s = fi.read()
-                s = re.sub(r'<bibl>hbbw', r'<bibl>HBBW', s, flags=re.S)
+                s = re.sub(r'[ ]+', r' ', s, flags=re.S)
                 with open(fp, 'w') as fo: fo.write(s)
