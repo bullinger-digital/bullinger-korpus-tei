ab41f021268b856bcf0215c026fd36cd984d1b46
bernsc <bernard.schroffenegger@uzh.ch>
2025-05-22 04:12:35 +0200

    linked bibliography ext.

--- scripts/patch_link_bib_entries.py
+++ scripts/patch_link_bib_entries.py
@@ -15,7 +15,7 @@ class BibTagger:
     def read_bibliography(self):
         with open(self.path_bib) as fi: s = fi.read()
         for b in re.findall(r'<bibl xml:id="([^"]*)">.*?<title>(.*?)</title>', s, flags=re.S):
-            self.bib[b[1]] = b[0]
+            self.bib[re.sub(r'\W', '', b[1]).lower()] = b[0]
 
     def set_bibl_refs(self):
         for f in os.listdir(self.ROOT):
@@ -23,9 +23,9 @@ class BibTagger:
                 p = os.path.join(self.ROOT, f)
                 with open(p) as fi: s = fi.read()
                 for b in re.findall(r'((<bibl>)(.*?)(</bibl>))', s, flags=re.S):
-                    if b[2] in self.bib:
+                    if re.sub(r'\W', '', b[2]).lower() in self.bib:
                         s = re.sub(re.escape(b[0]),
-                                r'<bibl source="'+self.bib[b[2]]+'">'+b[2]+b[3],
+                                r'<bibl source="'+self.bib[re.sub(r'\W', '', b[2]).lower()]+'">'+b[2]+b[3],
                                 s, flags=re.S)
                 with open(p, 'w') as fo: fo.write(s)
 
